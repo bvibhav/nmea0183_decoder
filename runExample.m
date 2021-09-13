771985cb8c38986fd4343385a6cb93fb08f06b07
Vibhav Bharti <bvibhav@yahoo.co.in>
2021-09-13 16:35:28 +0100

    Added ais 5 8 18

--- runExample.m
+++ runExample.m
@@ -1,11 +1,11 @@
-nmeaFile = 'someFile.ais';
+nmeaFile = 'AIS_SBARC_NMEA_200327-00.txt';
 nmeaFile = fopen(nmeaFile);
 
 tline = fgets(nmeaFile);
 while ischar(tline)
-    tStamp = tline(1:15);
-    tline(1:15) = [];
-    
+    tStamp = tline(1:24);
+    tline(1:24) = [];
+   
     if strcmp(tline(1:3),'!AI')
         fprintf('NMEA AIS Message: %s', tline(1:end-1));
         reportAIS = nmea_decodeAIS(tline, nmeaFile);
@@ -15,3 +15,21 @@ while ischar(tline)
     end
     tline = fgets(nmeaFile);
 end
+
+% nmeaFile = 'someFile.ais';
+% nmeaFile = fopen(nmeaFile);
+% 
+% tline = fgets(nmeaFile);
+% while ischar(tline)
+%     tStamp = tline(1:15);
+%     tline(1:15) = [];
+%     
+%     if strcmp(tline(1:3),'!AI')
+%         fprintf('NMEA AIS Message: %s', tline(1:end-1));
+%         reportAIS = nmea_decodeAIS(tline, nmeaFile);
+%     elseif strcmp(tline(1:3),'$GP')
+%         fprintf('NMEA GPS Message: %s', tline(1:end-1));
+%         reportGPS = nmea_decodeGPS(tline);
+%     end
+%     tline = fgets(nmeaFile);
+% end
